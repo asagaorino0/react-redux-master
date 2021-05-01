9cde289f6ae167d6e7efbed2cc3ab014310b0cf4
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-05-01 22:06:57 +0900

    [add]12 やっとNOMAL

--- src/features/counter/Todo.js
+++ src/features/counter/Todo.js
@@ -1,42 +1,41 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectTodo, add_todo, all_delete, del_todo } from './todoSlice';
+import { selectTodo, add_todo, all_delete, del_todo, DONE_LIST, check_list } from './todoSlice';
 import Checkbox from '@material-ui/core/Checkbox';
-// import FormGroup from '@material-ui/core/FormGroup';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import FormControl from '@material-ui/core/FormControl';
-// import FormLabel from '@material-ui/core/FormLabel';
-import Button from '@material-ui/core/Button';
 import styles from './Counter.module.css';
-import { bindActionCreators } from '@reduxjs/toolkit';
-
+// import { Button } from 'react-bootstrap'
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 export function Todo() {
   const todos = useSelector(selectTodo);
   const dispatch = useDispatch();
   const [event, setEvent] = useState('');
-  // const [checked, setChecked] = React.useState(true);
-  const [auth, setAuth] = React.useState(true);
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
-
+  const [complete, setComplete] = useState(false);
+  const [del, setDel] = useState('');
+  const [auth, setAuth] = React.useState(false);
   const addClick = () => {
-    dispatch(add_todo({ id: todos.length + 1, todo: event }))
+    dispatch(add_todo({ id: todos.length + 1, todo: event, complete, auth }))
   }
   const allDelete = () => {
     dispatch(all_delete());
   };
-
-  const delClick = () => {
-    const newdata = todos.filter(item => {
-      return item.id !== 2;
-    });
-    dispatch(del_todo({ newdata }))
-    console.log(newdata);
-    // return { newdata };
+  const delClick = (todo) => {
+    dispatch(del_todo({ todos, todo }))
+    // dispatch(del_todo({ todos }))---これは機能しなかった
+    console.log(del)
   };
+  const done = (id) => {
+    dispatch(DONE_LIST({ todos, id }))
+  };
+  const check = (id) => {
+    dispatch(check_list({ todos, id, auth }))
+  };
+
   return (
     <div>
+      <span className={styles.value}>Todo List</span>
+      <br />
       <input type="text" onChange={e => setEvent(e.target.value)} />
       <button
         aria-label="add_todo"
@@ -50,37 +49,48 @@ export function Todo() {
       >
         all_delete
         </button>
-      {/* <button
-        aria-label="all_delete"
-        onClick={() => dispatch(all_delete())}
-      >
-        all_delete
-        </button> */}
       <ul>
         {
           todos.map(item => {
-
-
-            // console.log(item)
             return (
-              <div key={item.id}>
+              <div key={item.id} >
                 <Checkbox
-                  onChange={handleChange}
+                  onChange={() => check(item.id)}
                   color="primary"
                   inputProps={{ 'aria-label': 'primary checkbox' }}
                 />
-                {item.id}: {item.todo}
-                <button
-                  className={styles.button}
-                  aria-label="Decrement value" onClick={delClick}>
-                  -
-                </button>
+                {/* {item.id}: */}
+                {item.todo}
+
+                {item.auth &&
+                  <IconButton aria-label="delete" >
+                    <DeleteIcon onClick={() => delClick(item.todo)} />
+                  </IconButton>
+                }
+
+
+
+
+
               </div>
             )
-
           })
-
-
+        }
+        <br />
+        <br />
+        ---違う仕組みの実装---
+                {
+          todos
+            .filter((todo) => todo.complete === false)
+            .map(item => {
+              return (
+                <div key={item.id}>{item.id}:{item.todo}
+                  <button
+                    className={styles.button}
+                    aria-label="Decrement value" onClick={() => done(item.id)}>-</button>
+                </div>
+              )
+            })
         }
       </ul>
     </div >
--- src/features/counter/todoSlice.js
+++ src/features/counter/todoSlice.js
@@ -1,8 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: [],
-  todo: [],
+  // value: [],
+  value: [
+    {
+      id: 1,
+      todo: "AAAAAA",
+      complete: false,
+      auth: false,
+    },
+    {
+      id: 2,
+      todo: "BBBBBBB",
+      complete: false,
+      auth: false,
+    },
+  ],
 };
 
 export const todoSlice = createSlice({
@@ -11,21 +24,73 @@ export const todoSlice = createSlice({
   reducers: {
     add_todo: (state, action) => {
       const event = action.payload
-      console.log(event);
+      console.log(event.id);//確認用
       state.value.push({ ...event })
     },
     del_todo: (state, action) => {
       const del = action.payload
-      console.log(del)
-      state.value = ({ ...del })
+      console.log('del.todo', del.todo)//確認用
+      console.log('?', del)//確認用
+      const todos = del.todos.filter(todos => {
+        return todos.todo !== del.todo;
+      });
+      state.value = todos
+      console.log('statevalue', state.value)//確認用
+    },
+    DONE_LIST: (state, action) => {
+      const done = action.payload
+      console.log('done', done, done.id)
+      const todos = done.todos.map(todos => {
+        console.log('todoid', todos.id)
+        if (todos.id === done.id)
+          return {
+            id: todos.id,
+            todo: todos.todo,
+            complete: true,
+          }
+        else {
+          return {
+            id: todos.id,
+            todo: todos.todo,
+            complete: todos.complete,
+          }
+        }
+      })
+      state.value = todos
+      console.log('statevalue', state.value)//確認用
+    },
+    check_list: (state, action) => {
+      const check = action.payload
+      console.log('check', check, check.id)
+      const todos = check.todos.map(todos => {
+        console.log('todoid', todos.id)
+        if (todos.id === check.id)
+          return {
+            id: todos.id,
+            todo: todos.todo,
+            complete: todos.complete,
+            auth: true,
+          }
+        else {
+          return {
+            id: todos.id,
+            todo: todos.todo,
+            complete: todos.complete,
+            auth: todos.auth,
+          }
+        }
+      })
+      state.value = todos
+      console.log('statevalue', state.value)//確認用
     },
     all_delete: (state) => {
       state.value = [];
+      console.log('statevalue', state.value)
     },
   }
 });
 
-export const { add_todo, del_todo, all_delete } = todoSlice.actions;
+export const { add_todo, del_todo, all_delete, DONE_LIST, check_list } = todoSlice.actions;
 
 export const selectTodo = (state) => state.todo.value;
 
